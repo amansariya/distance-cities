c80830f89e5f17041668c52f5c8b415a4e52e293
Aman Sariya <37979921+amansariya@users.noreply.github.com>
2024-05-04 13:46:34 -0700

    Update distance.js

--- docs/distance.js
+++ docs/distance.js
@@ -11,6 +11,6 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
         Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) * 
         Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // Distance in kilometers
+    const distance = R * c;
     return distance;
 }
